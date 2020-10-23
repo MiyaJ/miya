c7f6a2e28e2bb1f786a3f51a4619d0f6ca4a2d2f
CaiXiaowei <xiaowei.cai@ezhiyang.com>
2020-10-23 14:50:28 +0800

    feat: 整合rocketmq

--- miya-system/src/test/java/com/miya/system/MiyaSystemApplicationTests.java
+++ miya-system/src/test/java/com/miya/system/MiyaSystemApplicationTests.java
@@ -1,6 +1,13 @@
 package com.miya.system;
 
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,4 +17,18 @@ class MiyaSystemApplicationTests {
     void contextLoads() {
     }
 
+    @Autowired
+    DefaultMQProducer defaultMQProducer;
+
+    @Test
+    public void test_rocketmq() throws InterruptedException, RemotingException, MQClientException, MQBrokerException {
+        Message message = new Message();
+        message.setTopic("MyTopic");
+        message.setTags("TestTag");
+        message.setBody("hello rocketmq".getBytes());
+
+        SendResult send = defaultMQProducer.send(message);
+        System.out.println(send);
+
+    }
 }
