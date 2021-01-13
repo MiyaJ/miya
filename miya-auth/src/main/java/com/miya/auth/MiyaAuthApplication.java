2634fff281cc4ea24b9b9b3f3553ba7fb5e4cf81
CaiXiaowei <xiaowei.cai@ezhiyang.com>
2021-01-13 17:32:50 +0800

    feat: 新建miya-server 子服务聚合工程
    移动miya-system 到 miya-server 下

--- miya-auth/src/main/java/com/miya/auth/MiyaAuthApplication.java
+++ miya-auth/src/main/java/com/miya/auth/MiyaAuthApplication.java
@@ -5,7 +5,6 @@ import com.miya.annotation.MiyaCloudApplication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Caixiaowei-zy
