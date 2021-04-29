cf775983676342cb49c70ece734a5dea3a65590c
xiaowei.cai <540110865@qq.com>
2021-04-29 20:24:43 +0800

    fix: openfeign 调用, 在gateway转发是添加gateway token

--- miya-common/src/main/java/com/miya/configure/MiyaOAuth2FeignConfigure.java
+++ miya-common/src/main/java/com/miya/configure/MiyaOAuth2FeignConfigure.java
@@ -1,10 +1,12 @@
 package com.miya.configure;
 
+import com.miya.constant.MiyaConstant;
 import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.util.Base64Utils;
 
 /**
  * @author caixiaowei
@@ -19,6 +21,11 @@ public class MiyaOAuth2FeignConfigure {
     @Bean
     public RequestInterceptor oauth2FeignRequestInterceptor() {
         return requestTemplate -> {
+
+            // 添加 gateway Token
+            String gatewayToken = new String(Base64Utils.encode(MiyaConstant.GATEWAY_TOKEN_VALUE.getBytes()));
+            requestTemplate.header(MiyaConstant.GATEWAY_TOKEN_HEADER, gatewayToken);
+
             Object details = SecurityContextHolder.getContext().getAuthentication().getDetails();
             if (details instanceof OAuth2AuthenticationDetails) {
                 String authorizationToken = ((OAuth2AuthenticationDetails) details).getTokenValue();
